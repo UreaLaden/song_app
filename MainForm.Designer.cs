cd80a6d00eea808cb5bddf73741988dce601c344
Leaundrae Mckinney <pytechdrae@gmail.com>
2022-10-24 09:40:38 -0400

    Initial Commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Mckinney_CourseProject_CEIS209
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.titleText = new System.Windows.Forms.TextBox();
+            this.urlText = new System.Windows.Forms.TextBox();
+            this.urlLabel = new System.Windows.Forms.Label();
+            this.yearText = new System.Windows.Forms.TextBox();
+            this.yearLabel = new System.Windows.Forms.Label();
+            this.genreText = new System.Windows.Forms.TextBox();
+            this.genreLabel = new System.Windows.Forms.Label();
+            this.artistText = new System.Windows.Forms.TextBox();
+            this.artistLabel = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.outputText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(87, 78);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(86, 37);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Title:";
+            // 
+            // titleText
+            // 
+            this.titleText.Location = new System.Drawing.Point(191, 71);
+            this.titleText.Name = "titleText";
+            this.titleText.Size = new System.Drawing.Size(376, 44);
+            this.titleText.TabIndex = 1;
+            // 
+            // urlText
+            // 
+            this.urlText.Location = new System.Drawing.Point(191, 320);
+            this.urlText.Name = "urlText";
+            this.urlText.Size = new System.Drawing.Size(376, 44);
+            this.urlText.TabIndex = 7;
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Location = new System.Drawing.Point(87, 327);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(89, 37);
+            this.urlLabel.TabIndex = 6;
+            this.urlLabel.Text = "URL:";
+            // 
+            // yearText
+            // 
+            this.yearText.Location = new System.Drawing.Point(191, 260);
+            this.yearText.Name = "yearText";
+            this.yearText.Size = new System.Drawing.Size(376, 44);
+            this.yearText.TabIndex = 9;
+            // 
+            // yearLabel
+            // 
+            this.yearLabel.AutoSize = true;
+            this.yearLabel.Location = new System.Drawing.Point(87, 267);
+            this.yearLabel.Name = "yearLabel";
+            this.yearLabel.Size = new System.Drawing.Size(94, 37);
+            this.yearLabel.TabIndex = 8;
+            this.yearLabel.Text = "Year:";
+            // 
+            // genreText
+            // 
+            this.genreText.Location = new System.Drawing.Point(191, 197);
+            this.genreText.Name = "genreText";
+            this.genreText.Size = new System.Drawing.Size(376, 44);
+            this.genreText.TabIndex = 11;
+            // 
+            // genreLabel
+            // 
+            this.genreLabel.AutoSize = true;
+            this.genreLabel.Location = new System.Drawing.Point(87, 204);
+            this.genreLabel.Name = "genreLabel";
+            this.genreLabel.Size = new System.Drawing.Size(114, 37);
+            this.genreLabel.TabIndex = 10;
+            this.genreLabel.Text = "Genre:";
+            // 
+            // artistText
+            // 
+            this.artistText.Location = new System.Drawing.Point(191, 134);
+            this.artistText.Name = "artistText";
+            this.artistText.Size = new System.Drawing.Size(376, 44);
+            this.artistText.TabIndex = 13;
+            // 
+            // artistLabel
+            // 
+            this.artistLabel.AutoSize = true;
+            this.artistLabel.Location = new System.Drawing.Point(87, 141);
+            this.artistLabel.Name = "artistLabel";
+            this.artistLabel.Size = new System.Drawing.Size(100, 37);
+            this.artistLabel.TabIndex = 12;
+            this.artistLabel.Text = "Artist:";
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(191, 431);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(376, 93);
+            this.addButton.TabIndex = 14;
+            this.addButton.Text = "Add Song";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // outputText
+            // 
+            this.outputText.Location = new System.Drawing.Point(657, 35);
+            this.outputText.Multiline = true;
+            this.outputText.Name = "outputText";
+            this.outputText.Size = new System.Drawing.Size(782, 961);
+            this.outputText.TabIndex = 15;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1477, 1042);
+            this.Controls.Add(this.outputText);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.artistText);
+            this.Controls.Add(this.artistLabel);
+            this.Controls.Add(this.genreText);
+            this.Controls.Add(this.genreLabel);
+            this.Controls.Add(this.yearText);
+            this.Controls.Add(this.yearLabel);
+            this.Controls.Add(this.urlText);
+            this.Controls.Add(this.urlLabel);
+            this.Controls.Add(this.titleText);
+            this.Controls.Add(this.titleLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "MainForm";
+            this.Text = "Video Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.TextBox titleText;
+        private System.Windows.Forms.TextBox urlText;
+        private System.Windows.Forms.Label urlLabel;
+        private System.Windows.Forms.TextBox yearText;
+        private System.Windows.Forms.Label yearLabel;
+        private System.Windows.Forms.TextBox genreText;
+        private System.Windows.Forms.Label genreLabel;
+        private System.Windows.Forms.TextBox artistText;
+        private System.Windows.Forms.Label artistLabel;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox outputText;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mckinney_CourseProject_CEIS209
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder(outputText.Text);
+            string newLine = "\r\n";
+
+            sb.Append(titleText.Text);
+            sb.Append(newLine);
+            sb.Append(artistText.Text);
+            sb.Append(newLine);
+            sb.Append(genreText.Text);
+            sb.Append(newLine);
+            sb.Append(yearText.Text);
+            sb.Append(newLine);
+            sb.Append(urlText.Text);
+            sb.Append(newLine);
+            sb.Append(newLine);
+
+            outputText.Text = sb.ToString();
+
+            titleText.Clear();
+            artistText.Clear();
+            genreText.Clear();
+            yearText.Clear();
+            urlText.Clear();
+        }
+    }
+}
